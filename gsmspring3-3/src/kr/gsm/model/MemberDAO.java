f1672de21f7be10b11a60c87d7c1c875284d2df0
GSM <GSM@DESKTOP-UUM6D3I>
2021-03-19 16:20:50 +0900

    gsm3

---
+++ gsmspring3-3/src/kr/gsm/model/MemberDAO.java
@@ -0,0 +1,9 @@
+package kr.gsm.model;
+// JDBC->myBatis->JPA
+import java.sql.*;
+public class MemberDAO {
+   private Connection conn; // 연결객체
+   private PreparedStatement ps; // SQL문장을 전송객체
+   private ResultSet rs; // 결과집합을 저장하는 객체
+   
+}
